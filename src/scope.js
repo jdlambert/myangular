ff2d1f02a2c7d50a62fec463f57b43089578bf59
Jacob Lambert <jacob.lambert@gmail.com>
2017-04-07 16:32:58 +0800

    Add support for dependent watches

--- src/scope.js
+++ src/scope.js
@@ -15,11 +15,11 @@ Scope.prototype.$watch = function(watchFn, listenerFn) {
         last: initWatchVal
     };
     this.$$watchers.push(watcher);
-}
+};
 
-Scope.prototype.$digest = function() {
+Scope.prototype.$$digestOnce = function() {
     var self = this;
-    var newValue, oldValue;
+    var newValue, oldValue, dirty;
     _.forEach(this.$$watchers, function(watcher) {
         newValue = watcher.watchFn(self);
         oldValue = watcher.last;
@@ -30,8 +30,17 @@ Scope.prototype.$digest = function() {
                 (oldValue === initWatchVal ? newValue : oldValue), 
                 self
             );
+            dirty = true;
         }
     });
-}
+    return dirty;
+};
+
+Scope.prototype.$digest = function() {
+    var dirty;
+    do {
+        dirty = this.$$digestOnce();
+    } while (dirty);
+};
 
 module.exports = Scope;
