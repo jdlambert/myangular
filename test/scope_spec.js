e5337a1bfbca2fe5e99915bc0c8480d687df4883
Jacob Lambert <jacob.lambert@gmail.com>
2017-04-08 11:08:46 +0800

    Remove corner case when adding a new watch

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -169,6 +169,26 @@ describe('Scope', function() {
             expect(watchExecutions).toBe(301);
         });
 
+        it('does not end digest so that new watches are not run', function() {
+            scope.aValue = 'abc';
+            scope.counter = 0;
+
+            scope.$watch(
+                function(scope) { return scope.aValue; },
+                function(newValue, oldValue, scope) {
+                    scope.$watch(
+                        function(scope) { return scope.aValue; },
+                        function(newValue, oldValue, scope) {
+                            scope.counter++;
+                        }
+                    );
+                }
+            );
+
+            scope.$digest();
+            expect(scope.counter).toBe(1);
+        })
+
     });
 
 });
