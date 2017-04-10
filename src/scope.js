5213a76972821bd78ee8901992d48ac3257bb60f
Jacob Lambert <jacob.lambert@gmail.com>
2017-04-10 12:47:41 +0800

    Parents digest their children

--- src/scope.js
+++ src/scope.js
@@ -46,31 +46,37 @@ Scope.prototype.$watch = function(watchFn, listenerFn, valueEq) {
 
 Scope.prototype.$$digestOnce = function() {
     var self = this;
-    var newValue, oldValue, dirty;
-    _.forEachRight(this.$$watchers, function(watcher) {
-        try {
-            if (watcher) { // watcher may have already been destroyed
-                newValue = watcher.watchFn(self);
-                oldValue = watcher.last;
-                if (!self.$$areEqual(newValue, oldValue, watcher.valueEq)) {
-                    self.$$lastDirtyWatch = watcher;
-                    watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
-                    watcher.listenerFn(
-                        newValue, 
-                        (oldValue === initWatchVal ? newValue : oldValue), 
-                        self
-                    );
-                    dirty = true;
-                } else if (self.$$lastDirtyWatch === watcher) {
-                    // explicitly returning false in a _.forEach loop causes
-                    // LoDash to short-circuit the loop and exit immediately
-                    return false; 
+    var dirty;
+    var continueLoop = true;
+    this.$$everyScope(function(scope) {
+        var newValue, oldValue;
+        _.forEachRight(scope.$$watchers, function(watcher) {
+            try {
+                if (watcher) { // watcher may have already been destroyed
+                    newValue = watcher.watchFn(scope);
+                    oldValue = watcher.last;
+                    if (!scope.$$areEqual(newValue, oldValue, watcher.valueEq)) {
+                        self.$$lastDirtyWatch = watcher;
+                        watcher.last = (watcher.valueEq ? _.cloneDeep(newValue) : newValue);
+                        watcher.listenerFn(
+                            newValue, 
+                            (oldValue === initWatchVal ? newValue : oldValue), 
+                            scope
+                        );
+                        dirty = true;
+                    } else if (self.$$lastDirtyWatch === watcher) {
+                        continueLoop = false;
+                        // explicitly returning false in a _.forEach loop causes
+                        // LoDash to short-circuit the loop and exit immediately
+                        return false; 
+                    }
                 }
+            } catch (e) {
+                console.error(e);
             }
-        } catch (e) {
-            console.error(e);
-        }
-    });
+        });
+        return continueLoop;
+    })
     return dirty;
 };
 
@@ -231,5 +237,15 @@ Scope.prototype.$watchGroup = function(watchFns, listenerFn) {
     };
 
 };
+
+Scope.prototype.$$everyScope = function(fn) {
+    if (fn(this)) {
+        return this.$$children.every(function(child) {
+            return child.$$everyScope(fn);
+        });
+    } else {
+        return false;
+    }
+}
 
 module.exports = Scope;
