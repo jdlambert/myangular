1f0d7c558508773b46b73e301de941494b920951
Jacob Lambert <jacob.lambert@gmail.com>
2017-04-13 12:52:56 +0800

    Add detection of objects with length property

--- src/scope.js
+++ src/scope.js
@@ -79,7 +79,7 @@ Scope.prototype.$watchCollection = function(watchFn, listenerFn) {
         newValue = watchFn(scope);
 
         if (_.isObject(newValue)) {
-            if (_.isArrayLike(newValue)) {
+            if (isArrayLike(newValue)) {
                 if (!_.isArray(oldValue)) {
                     changeCount++;
                     oldValue = [];
@@ -148,7 +148,8 @@ function isArrayLike(obj) {
         return false;
     }
     var length = obj.length;
-    return _.isNumber(length);
+    return length === 0 ||
+        (_.isNumber(length) && length > 0 && (length - 1) in obj);
 }
 
 Scope.prototype.$$digestOnce = function() {
