b3df925c2dedfbd0a2faca2e96a2a5a90251ee4e
Jacob Lambert <jacob.lambert@gmail.com>
2017-04-13 13:12:01 +0800

    Tweak first-run oldValue in watchCollection

--- test/scope_spec.js
+++ test/scope_spec.js
@@ -1694,6 +1694,22 @@ describe('Scope', function() {
             expect(oldValueGiven).toEqual({a: 1, b: 2});
         });
 
+        it('uses the new value as the old value on the first digest', function() {
+            scope.aValue = {a: 1, b: 2};
+            var oldValueGiven;
+
+            scope.$watchCollection(
+                function(scope) { return scope.aValue; },
+                function(newValue, oldValue, scope) {
+                    oldValueGiven = oldValue;
+                }
+            );
+
+            scope.$digest();
+
+            expect(oldValueGiven).toEqual({a: 1, b: 2});
+        })
+
     });
 
 });
