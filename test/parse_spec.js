de0b008df21554670fc83c58300ea536b3a4899a
Jacob Lambert <jacob.lambert@gmail.com>
2017-04-16 23:02:34 +0800

    Add support for scientific notatin

--- test/parse_spec.js
+++ test/parse_spec.js
@@ -18,6 +18,30 @@ describe('parse', function() {
     it('can parse a floating point number without an integer part', function() {
         var fn = parse('.42');
         expect(fn()).toBe(0.42);
-    })
+    });
 
+    it('can parse a number in scientific notation', function() {
+        var fn = parse('42e3');
+        expect(fn()).toBe(42000);
+    });
+
+    it('can parse a number in scientific notation with a float coefficient', function() {
+        var fn = parse('.42e2');
+        expect(fn()).toBe(42);
+    });
+
+    it('can parse a number in scientific notation with negative exponents', function() {
+        var fn = parse('4200e-2');
+        expect(fn()).toBe(42);
+    });
+
+    it('can parse a number in scientific notation with the + sign', function() {
+        var fn = parse('.42e+2');
+        expect(fn()).toBe(42);
+    });
+
+    it('can parse upper case scientific notation', function() {
+        var fn = parse('.42E2');
+        expect(fn()).toBe(42);
+    });
 });
