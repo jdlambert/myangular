66b367b7ddb9e4cae0fb3fc3239cb6179efd3721
Jacob Lambert <jacob.lambert@gmail.com>
2017-04-17 17:25:59 +0800

    Added parsing of arrays with trailing commas

--- src/parse.js
+++ src/parse.js
@@ -230,6 +230,9 @@ AST.prototype.arrayDeclaration = function() {
     var elements = [];
     if (!this.peek(']')) {
         do {
+            if (this.peek(']')) {
+                break
+            }
             elements.push(this.primary());
         } while (this.expect(','));
     }
