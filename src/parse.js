2aada5cb5ca1c471b3d27da030403ae078bb0371
Jacob Lambert <jacob.lambert@gmail.com>
2017-04-18 13:21:40 +0800

    Add object attribute lookup

--- src/parse.js
+++ src/parse.js
@@ -36,7 +36,7 @@ Lexer.prototype.lex = function(text) {
             this.readNumber();
         } else if (this.is('\'"')) {
            this.readString(this.ch); 
-        } else if (this.is('[],{}:')) {
+        } else if (this.is('[],{}:.')) {
             this.tokens.push({
                 text: this.ch
             });
@@ -187,6 +187,7 @@ AST.ObjectExpression = 'ObjectExpression';
 AST.Property = 'Property';
 AST.Identifier = 'Identifier';
 AST.ThisExpression = 'ThisExpression';
+AST.MemberExpression = 'MemberExpression';
 
 AST.prototype.constants = {
     'null': {type: AST.Literal, value: null},
@@ -205,17 +206,26 @@ AST.prototype.program = function() {
 };
 
 AST.prototype.primary = function() {
+    var primary;
     if(this.expect('[')) {
-        return this.arrayDeclaration();
+        primary = this.arrayDeclaration();
     } else if (this.expect('{')) {
-        return this.object();
+        primary = this.object();
     } else if (this.constants.hasOwnProperty(this.tokens[0].text)) {
-        return this.constants[this.consume().text];
+        primary = this.constants[this.consume().text];
     } else if (this.peek().identifier) {
-        return this.identifier();
+        primary = this.identifier();
     } else {
-        return this.constant();
+        primary = this.constant();
     }
+    if (this.expect('.')) {
+        primary = {
+            type: AST.MemberExpression,
+            object: primary,
+            property: this.identifier()
+        };
+    }
+    return primary;
 };
 
 AST.prototype.peek = function(e) {
@@ -311,6 +321,7 @@ ASTCompiler.prototype.compile = function(text) {
 };
 
 ASTCompiler.prototype.recurse = function(ast) {
+    var intoId;
     switch (ast.type) {
         case AST.Program:
             this.state.body.push('return ', this.recurse(ast.body), ';');
@@ -332,13 +343,19 @@ ASTCompiler.prototype.recurse = function(ast) {
             }, this));
             return '{' + properties.join(',') + '}';
         case AST.Identifier:
-            var intoId = this.nextId();
+            intoId = this.nextId();
             this.if_('s', this.assign(intoId, this.nonComputedMember('s', ast.name)));;
             return intoId;
         // A non-computed lookup is of the a.b type, as opposed to the computed a[b] type
         // That is, b is simply an identifier, it cannot be an expression
         case AST.ThisExpression:
             return 's';
+        case AST.MemberExpression:
+            intoId = this.nextId(); // so as to return undefined if the property or attribute is undefined
+            var left = this.recurse(ast.object);
+            this.if_(left,
+                this.assign(intoId, this.nonComputedMember(left, ast.property.name)));
+            return intoId;
     }
 };
 
