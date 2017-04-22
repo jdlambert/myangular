3ce6a9aa15989f8cb6097706d18e3129d358f593
Jacob Lambert <jacob.lambert@gmail.com>
2017-04-22 10:48:15 +0800

    Add simple predicate filtering and string filtering of arrays

--- src/filter_filter.js
+++ src/filter_filter.js
@@ -1,9 +1,27 @@
 'use strict';
 
-function filterFilter() {
-    return function() {
+var _ = require('lodash');
 
+function filterFilter() {
+    return function(array, filterExpr) {
+        var predicateFn;
+        if(_.isFunction(filterExpr)) {
+            predicateFn = filterExpr;
+        } else if (_.isString(filterExpr)) {
+            predicateFn = createPredicateFn(filterExpr);
+        } else {
+            return array;
+        }
+        return _.filter(array, predicateFn);
     };
+}
+
+function createPredicateFn(expression) {
+    return function predicateFn(item) {
+        var actual = item.toLowerCase();
+        var expected = expression.toLowerCase();
+        return actual.indexOf(expected) !== -1;
+    }
 }
 
 module.exports = filterFilter;
--- test/filter_filter_spec.js
+++ test/filter_filter_spec.js
@@ -1,11 +1,49 @@
 'use strict';
 
+var parse = require('../src/parse');
 var filter = require('../src/filter').filter;
 
 describe('filter filter', function() {
 
     it('is available', function() {
         expect(filter('filter')).toBeDefined();
+    });
+
+    it('can filter an array with a predicate function', function() {
+        var fn = parse('[1, 2, 3, 4] | filter:isOdd');
+        var scope = {
+            isOdd: function(n) {
+                return n % 2 !== 0;
+            }
+        };
+        expect(fn(scope)).toEqual([1, 3]);
+    });
+
+    it('can filter an array of strings with a string', function() {
+        var fn = parse('arr | filter:"a"');
+        expect(fn({arr: ['a', 'b', 'a']})).toEqual(['a', 'a']);
+    });
+
+    it('can filter an array of strings with substring matching', function() {
+        var fn = parse('arr | filter:"o"');
+        expect(fn({arr: ['quick', 'brown', 'fox']})).toEqual(['brown', 'fox']);
+    });
+
+    it('filters an array of strings ignoring case', function() {
+        var fn = parse('arr | filter:"o"');
+        expect(fn({arr: ['quick', 'BROWN', 'fox']})).toEqual(['BROWN', 'fox']);
+    });
+
+    it('filters an array of objects where any value matches', function() {
+        var fn = parse('arr | filter:"o"');
+        expect(fn({arr: [
+            {firstName: 'John', lastName: 'Brown'},
+            {firstName: 'Jane', lastName: 'Fox'},
+            {firstName: 'Mary', lastName: 'Quick'},
+        ]})).toEqual([
+            {firstName: 'John', lastName: 'Brown'},
+            {firstName: 'Jane', lastName: 'Fox'}
+        ]);
     });
 
 });
