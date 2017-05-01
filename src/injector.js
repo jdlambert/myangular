dcfa84fcbcf4e04413281c53a42cd7a58c27db22
Jacob Lambert <jacob.lambert@gmail.com>
2017-05-01 12:57:10 +0800

    Build lowest-level dependency injection for constants

--- src/injector.js
+++ src/injector.js
@@ -16,6 +16,17 @@ function createInjector(modulesToLoad) {
         }
     };
 
+    function invoke(fn) {
+        var args = _.map(fn.$inject, function(token) {
+            if (_.isString(token)) {
+                return cache[token];
+            } else {
+                throw 'Incorrect injection token! Expected a string, got ' + token;
+            }
+        });
+        return fn.apply(null, args);
+    }
+
     _.forEach(modulesToLoad, function loadModule(moduleName) {
         if (!loadedModules.hasOwnProperty(moduleName)) {
             loadedModules[moduleName] = true;
@@ -35,7 +46,8 @@ function createInjector(modulesToLoad) {
         },
         get: function(key) {
             return cache[key];
-        }
+        },
+        invoke: invoke
     };
 };
 
