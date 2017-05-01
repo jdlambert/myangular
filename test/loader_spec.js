d79092a893b38108e2dfe9f0b9bf188af346bab6
Jacob Lambert <jacob.lambert@gmail.com>
2017-05-01 12:03:26 +0800

    Add module getter

--- test/loader_spec.js
+++ test/loader_spec.js
@@ -56,4 +56,24 @@ describe('modules', function() {
         expect(myModule.requires).toEqual(['myOtherModule']);
     });
 
+    it('allows getting a module', function() {
+        var myModule = window.angular.module('myModule', []);
+        var gotModule = window.angular.module('myModule');
+
+        expect(gotModule).toBeDefined();
+        expect(gotModule).toBe(myModule);
+    });
+
+    it('throws when trying to get a nonexistant module', function() {
+        expect(function() {
+            window.angular.module('myModule');
+        }).toThrow();
+    });
+
+    it('does not allow for a module to be called hasOwnProperty', function() {
+        expect(function() {
+            window.angular.module('hasOwnProperty', []);
+        }).toThrow();
+    });
+
 });
