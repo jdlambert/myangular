03428bf4db5dd3e24774730391b91dcae3950007
Jacob Lambert <jacob.lambert@gmail.com>
2017-05-01 11:29:21 +0800

    Create singleton module loader

---
+++ test/loader_spec.js
@@ -0,0 +1,23 @@
+'use strict';
+
+var setupModuleLoader = require('../src/loader');
+
+describe('setupModuleLoader', function() {
+
+    beforeEach(function() {
+        delete window.angular;
+    });
+
+    it('exposes angular on the window', function() {
+        setupModuleLoader(window);
+        expect(window.angular).toBeDefined();
+    });
+
+    it('creates angular just once', function() {
+        setupModuleLoader(window);
+        var ng = window.angular;
+        setupModuleLoader(window);
+        expect(window.angular).toBe(ng);
+    })
+
+});
