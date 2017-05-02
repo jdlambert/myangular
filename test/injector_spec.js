7a34252497c4024bc56a0f541deec64d5909be6b
Jacob Lambert <jacob.lambert@gmail.com>
2017-05-02 12:55:35 +0800

    Implement singleton providers

--- test/injector_spec.js
+++ test/injector_spec.js
@@ -365,4 +365,13 @@ describe('provider', function() {
         expect(injector.get('b')).toBe(3);
     });
 
+    it('instantiates a dependency only once', function () {
+        var module = window.angular.module('myModule', []);
+        module.provider('a', {$get: function() { return {}; }});
+
+        var injector = createInjector(['myModule']);
+
+        expect(injector.get('a')).toBe(injector.get('a'));
+    });
+
 });
