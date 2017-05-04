d0b66f2e00b3dded6b77e8460303018508623208
Jacob Lambert <jacob.lambert@gmail.com>
2017-05-04 14:43:37 +0800

    Implement identity-based ids in hashKey

--- test/hash_map_spec.js
+++ test/hash_map_spec.js
@@ -5,6 +5,11 @@ describe('hash', function() {
 
     describe('hashKey', function() {
 
+        /* The hashKey function is not strictly a hash key function at all, in
+            the same sense as the Java and Ruby methods are. In the case of object
+            and data structures, it is just a unique identifier that is based on
+            object identity rather than value */
+
         it('is undefined:undefined for undefined', function() {
             expect(hashKey(undefined)).toEqual('undefined:undefined');
         });
@@ -28,6 +33,55 @@ describe('hash', function() {
         it('is string:42 for "42"', function() {
             expect(hashKey('42')).toEqual('string:42');
         });
+
+        it('is object:[unique id] for objects', function() {
+            expect(hashKey({})).toMatch(/^object:\S+$/);
+        });
+
+        it('is the same key when asked for the same object many times', function() {
+            var obj = {};
+            expect(hashKey(obj)).toEqual(hashKey(obj));
+        });
+
+        it('does not change when object value changes', function() {
+            var obj = {a: 42};
+            var hash1 = hashKey(obj);
+            obj.a = 43;
+            var hash2 = hashKey(obj);
+            expect(hash1).toEqual(hash2);
+        });
+
+        it('is not the same for different object even with the same value', function() {
+            var obj1 = {a: 42};
+            var obj2 = {a: 42};
+            expect(hashKey(obj1)).not.toEqual(hashKey(obj2));
+        });
+
+        it('is function:[unique id] for objects', function() {
+            var fn = function(a) { return a; };
+            expect(hashKey(fn)).toMatch(/^function:\S+$/);
+        });
+
+        it('is the same key when asked for the same object many times', function() {
+            var fn = function() { };
+            expect(hashKey(fn)).toEqual(hashKey(fn));
+        });
+
+        it('is not the same for two identical functions', function() {
+            var fn1 = function() { return 42; };
+            var fn2 = function() { return 42; };
+            expect(hashKey(fn1)).not.toEqual(hashKey(fn2));
+        });
+
+        it('stores the hash key in the $$hashKey attribute', function() {
+            var obj = {a: 42};
+            var hash = hashKey(obj);
+            expect(obj.$$hashKey).toEqual(hash.match(/^object:(\S+)$/)[1]);
+        });
+
+        it('uses preassigned $$hashKey', function() {
+            expect(hashKey({$$hashKey: 42})).toEqual('object:42');
+        })
 
     });
 });
