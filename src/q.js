27347f660f332892e6db339703bf8d9f2b4f3e9c
Jacob Lambert <jacob.lambert@gmail.com>
2017-05-08 19:40:17 +0800

    Start building promise rejection

--- src/q.js
+++ src/q.js
@@ -10,9 +10,9 @@ function $QProvider() {
             this.$$state = {};
         }
 
-        Promise.prototype.then = function(onFulfilled) {
+        Promise.prototype.then = function(onFulfilled, onRejected) {
             this.$$state.pending = this.$$state.pending || [];
-            this.$$state.pending.push(onFulfilled);
+            this.$$state.pending.push([null, onFulfilled, onRejected]);
             if (this.$$state.status > 0) {
                 scheduleProcessQueue(this.$$state);
             }
@@ -32,6 +32,15 @@ function $QProvider() {
             scheduleProcessQueue(this.promise.$$state);
         };
 
+        Deferred.prototype.reject = function(reason) {
+            if (this.promise.$$state.status) {
+                return;
+            }
+            this.promise.$$state.value = reason;
+            this.promise.$$state.status = 2;
+            scheduleProcessQueue(this.promise.$$state);
+        };
+
         function scheduleProcessQueue(state) {
             $rootScope.$evalAsync(function() {
                 processQueue(state);
@@ -41,8 +50,9 @@ function $QProvider() {
         function processQueue(state) {
             var pending = state.pending;
             state.pending = undefined;
-            _.forEach(pending, function(onFulfilled) {
-                onFulfilled(state.value);
+            _.forEach(pending, function(handlers) {
+                var fn = handlers[state.status];
+                fn(state.value);
             })
         }
 
