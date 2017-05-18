0c8d296c5a2b646507f7559fe2fd48af65a6008f
Jacob Lambert <jacob.lambert@gmail.com>
2017-05-18 13:31:16 +0800

    Merge default and config headers

--- src/http.js
+++ src/http.js
@@ -3,14 +3,54 @@
 var _ = require('lodash');
 
 function $HttpProvider() {
+
+    var defaults = this.defaults = {
+        headers: {
+            common: {
+                Accept: 'application/json, text/plain, */*'
+            },
+            post: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            put: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            patch: {
+                'Content-Type': 'application/json;charset=utf-8'
+            }
+        }
+    };
+
+    function mergeHeaders(config) {
+        var reqHeaders = _.extend(
+            {},
+            config.headers
+        );
+        var defHeaders = _.extend(
+            {},
+            defaults.headers.common,
+            defaults.headers[(config.method || 'get').toLowerCase()]
+        )
+        _.forEach(defHeaders, function(value, key) {
+            var headerExists = _.some(reqHeaders, function(v, k) {
+                return k.toLowerCase() === key.toLowerCase();
+            });
+            if (!headerExists) {
+                reqHeaders[key] = value;
+            }
+        });
+        return reqHeaders;
+    }
+
     this.$get = ['$httpBackend', '$q', '$rootScope', function($httpBackend, $q, $rootScope) {
 
-        return function $http(requestConfig) {
+        function $http(requestConfig) {
             var deferred = $q.defer();
 
             var config = _.extend({
                 method: 'GET'
             }, requestConfig);
+            config.headers = mergeHeaders(requestConfig);
 
             function isSuccess(status) {
                 return status >= 200 && status < 300;
@@ -29,9 +69,19 @@ function $HttpProvider() {
                 }
             }
 
-            $httpBackend(config.method, config.url, config.data, done);
+            $httpBackend(
+                config.method,
+                config.url,
+                config.data,
+                done,
+                config.headers
+            );
             return deferred.promise;
         };
+
+        $http.defaults = defaults;
+
+        return $http;
 
     }];
 }
--- test/http_spec.js
+++ test/http_spec.js
@@ -115,6 +115,86 @@ describe('$http', function() {
         });
         expect(requests.length).toBe(1);
         expect(requests[0].method).toBe('GET');
-    })
+    });
+
+    it('sets headers on request', function() {
+        $http({
+            url: 'http://teropa.info',
+            headers: {
+                'Accept': 'text/plain',
+                'Cache-Control': 'no-cache'
+            }
+        });
+        expect(requests.length).toBe(1);
+        expect(requests[0].requestHeaders.Accept).toBe('text/plain');
+        expect(requests[0].requestHeaders['Cache-Control']).toBe('no-cache');
+    });
+
+    it('sets default headers on request', function() {
+        $http({
+            url: 'http://teropa.info'
+        });
+        expect(requests.length).toBe(1);
+        expect(requests[0].requestHeaders.Accept).toBe(
+            'application/json, text/plain, */*');
+    });
+
+    it('sets method-specific default headers on requests', function() {
+        $http({
+            method: 'POST',
+            url: 'http://teropa.info',
+            data: '42'
+        });
+        expect(requests.length).toBe(1);
+        expect(requests[0].requestHeaders['Content-Type']).toBe(
+            'application/json;charset=utf-8');
+    });
+
+    it('exposes default headers for overriding', function() {
+        $http.defaults.headers.post['Content-Type'] = 'text/plain;charset=utf-8';
+        $http({
+            method: 'POST',
+            url: 'http://teropa.info',
+            data: '42'
+        });
+        expect(requests.length).toBe(1);
+        expect(requests[0].requestHeaders['Content-Type']).toBe(
+            'text/plain;charset=utf-8'
+        );
+    });
+
+    it('exposes default headers through provider', function() {
+        var injector = createInjector(['ng', function($httpProvider) {
+            $httpProvider.defaults.headers.post['Content-Type'] =
+                'text/plain;charset=utf-8';
+        }]);
+        $http = injector.get('$http');
+
+        $http({
+            method: 'POST',
+            url: 'http://teropa.info',
+            data: '42'
+        });
+        expect(requests.length).toBe(1);
+        expect(requests[0].requestHeaders['Content-Type']).toBe(
+            'text/plain;charset=utf-8'
+        );
+    });
+
+    it('merges default headers case-insensitively', function() {
+        $http({
+            method: 'POST',
+            url: 'http://teropa.info',
+            data: '42',
+            headers: {
+                'content-type': 'text/plain;charset=utf-8'
+            }
+        });
+        expect(requests.length).toBe(1);
+        expect(requests[0].requestHeaders['content-type']).toBe(
+            'text/plain;charset=utf-8'
+        );
+        expect(requests[0].requestHeaders['Content-Type']).toBeUndefined();
+    });
         
 });
