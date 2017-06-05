5f46498738a7137dc54abe6b3a3c0571d9c52979
Jacob Lambert <jacob.lambert@gmail.com>
2017-06-05 21:02:12 +0800

    Add parsing for json resonses

--- src/http.js
+++ src/http.js
@@ -18,8 +18,48 @@ function $HttpProvider() {
             patch: {
                 'Content-Type': 'application/json;charset=utf-8'
             }
-        }
+        },
+        transformRequest: [function(data) {
+            if (_.isObject(data) && !isBlob(data) &&
+                    !isFile(data) && !isFormData(data)) {
+                return JSON.stringify(data);
+            } else {
+                return data;
+            }
+        }],
+        transformResponse: [defaultHttpResponseTransform]
     };
+
+    function isBlob(object) {
+        return object.toString() === '[object Blob]';
+    }
+
+    function isFile(object) {
+        return object.toString() === '[object File]';
+    }
+
+    function isFormData(object) {
+        return object.toString() === '[object FormData]';
+    }
+
+    function defaultHttpResponseTransform(data, headers) {
+        if (_.isString(data)) {
+            var contentType = headers('Content-Type');
+            if ((contentType && contentType.indexOf('application/json') === 0) ||
+                isJsonLike(data)) {
+                return JSON.parse(data);
+            }
+        }
+        return data;
+    }
+
+    function isJsonLike(data) {
+        if (data.match(/^\{(?!\{)/)) {
+            return data.match(/\}$/);
+        } else if (data.match(/^\[/)) {
+            return data.match(/\]$/);
+        }
+    }
 
     function mergeHeaders(config) {
         var reqHeaders = _.extend(
