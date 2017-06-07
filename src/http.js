179b52fa8199eb39dc62ba05fe0720dccc32ca9d
Jacob Lambert <jacob.lambert@gmail.com>
2017-06-07 12:38:43 +0800

    Add useApplyAsync feature to http provider

--- src/http.js
+++ src/http.js
@@ -6,6 +6,16 @@ var _ = require('lodash');
 function $HttpProvider() {
 
     var interceptorFactories = this.interceptors = [];
+    var useApplyAsync = false;
+
+    this.useApplyAsync = function(value) {
+        if (_.isUndefined(value)) {
+            return useApplyAsync;
+        } else {
+            useApplyAsync = !!value;
+            return this;
+        }
+    };
 
     var defaults = this.defaults = {
         headers: {
@@ -171,15 +181,24 @@ function $HttpProvider() {
 
             function done(status, response, headersString, statusText) {
                 status = Math.max(status, 0);
-                deferred[isSuccess(status) ? 'resolve' : 'reject']({
-                    status: status,
-                    data: response,
-                    statusText: statusText,
-                    headers: headersGetter(headersString),
-                    config: config
-                });
-                if (!$rootScope.$$phase) {
-                    $rootScope.$apply();
+
+                function resolvePromise() {
+                    deferred[isSuccess(status) ? 'resolve' : 'reject']({
+                        status: status,
+                        data: response,
+                        statusText: statusText,
+                        headers: headersGetter(headersString),
+                        config: config
+                    });
+                }
+
+                if (useApplyAsync) {
+                    $rootScope.$applyAsync(resolvePromise);
+                } else {
+                    resolvePromise();
+                    if (!$rootScope.$$phase) {
+                        $rootScope.$apply();
+                    }
                 }
             }
 
