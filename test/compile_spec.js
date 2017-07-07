dc92665005d0d1e1d7fd8da5a5a7a0a35662941b
Jacob Lambert <jacob.lambert@gmail.com>
2017-07-07 18:31:29 +0800

    Require directives using objects

--- test/compile_spec.js
+++ test/compile_spec.js
@@ -2184,6 +2184,77 @@ describe('$compile', function() {
                 expect(gotMyControllers[1] instanceof MyOtherController).toBe(true);
             });
         });
+ 
+        it('can be required as an object', function() {
+            function MyController() { }
+            function MyOtherController() { }
+            var gotMyControllers;
+            var injector = createInjector(['ng',
+                function($compileProvider) {
+                    $compileProvider.directive('myDirective', function() {
+                        return {
+                            scope: true,
+                            controller: MyController
+                        };
+                    });
+                    $compileProvider.directive('myOtherDirective', function() {
+                        return {
+                            scope: true,
+                            controller: MyOtherController
+                        };
+                    });
+                    $compileProvider.directive('myThirdDirective', function() {
+                        return {
+                            require: {
+                                myDirective: 'myDirective',
+                                myOtherDirective: 'myOtherDirective'
+                            },
+                            link: function(scope, element, attrs, controllers) {
+                                gotMyControllers = controllers
+                            }
+                        };
+                    });
+                }
+            ]);
+            injector.invoke(function($compile, $rootScope) {
+                var el = $('<div my-directive my-other-directive my-third-directive></div>');
+                $compile(el)($rootScope);
+                expect(gotMyControllers).toBeDefined();
+                expect(gotMyControllers.myDirective instanceof MyController).toBe(true);
+                expect(gotMyControllers.myOtherDirective instanceof MyOtherController).toBe(true);
+            });
+        });
+
+        it('can be required as an object with values omitted', function() {
+            function MyController() { }
+            var gotControllers;
+            var injector = createInjector(['ng',
+                function($compileProvider) {
+                    $compileProvider.directive('myDirective', function() {
+                        return {
+                            scope: true,
+                            controller: MyController
+                        };
+                    });
+                    $compileProvider.directive('myOtherDirective', function() {
+                        return {
+                            require: {
+                                myDirective: ''
+                            },
+                            link: function(scope, element, attrs, controllers) {
+                                gotControllers = controllers;
+                            }
+                        };
+                    });
+                }
+            ]);
+            injector.invoke(function($compile, $rootScope) {
+                var el = $('<div my-directive my-other-directive></div>');
+                $compile(el)($rootScope);
+                expect(gotControllers).toBeDefined();
+                expect(gotControllers.myDirective instanceof MyController).toBe(true);
+            });
+        });
 
     });
 
