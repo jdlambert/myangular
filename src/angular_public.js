bceaeec3a122709bc2fed6300eead4f76d1b3321
Jacob Lambert <jacob.lambert@gmail.com>
2017-07-08 19:10:22 +0800

    Implement ngController

--- src/angular_public.js
+++ src/angular_public.js
@@ -19,6 +19,8 @@ function publishExternalAPI() {
     ngModule.provider('$httpBackend', require('./http_backend'));
     ngModule.provider('$compile', require('./compile'));
     ngModule.provider('$controller', require('./controller'));
+    ngModule.directive('ngController',
+        require('./directives/ng_controller'));
 }
 
 module.exports = publishExternalAPI;
