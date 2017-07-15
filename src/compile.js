c4386d86fc7af2f72832eec60e97bb9a943ca2be
Jacob Lambert <jacob.lambert@gmail.com>
2017-07-15 12:37:01 +0800

    Transcluded scopes

--- src/compile.js
+++ src/compile.js
@@ -309,14 +309,29 @@ function $CompileProvider($provide) {
         _.forEach(linkFns, function(linkFn) {
           var node = stableNodeList[linkFn.idx];
           if (linkFn.nodeLinkFn) {
+            var childScope;
             if (linkFn.nodeLinkFn.scope) {
-              scope = scope.$new();
-              $(node).data('$scope', scope);
+              childScope = scope.$new();
+              $(node).data('$scope', childScope);
+            } else {
+              childScope = scope;
             }
+
+            var boundTranscludeFn;
+            if (linkFn.nodeLinkFn.transcludeOnThisElement) {
+              boundTranscludeFn = function(transcludedScope, containingScope) {
+                if (!transcludedScope) {
+                  transcludedScope = scope.$new(false, containingScope);
+                }
+                return linkFn.nodeLinkFn.transclude(transcludedScope);
+              };
+            }
+
             linkFn.nodeLinkFn(
               linkFn.childLinkFn,
-              scope,
-              node
+              childScope,
+              node,
+              boundTranscludeFn
             );
           } else {
             linkFn.childLinkFn(
@@ -591,7 +606,7 @@ function $CompileProvider($provide) {
         }
       });
 
-      function nodeLinkFn(childLinkFn, scope, linkNode) {
+      function nodeLinkFn(childLinkFn, scope, linkNode, boundTranscludeFn) {
         var $element = $(linkNode);
 
         var isolateScope;
@@ -655,13 +670,17 @@ function $CompileProvider($provide) {
           }
         });
 
+        function scopeBoundTranscludeFn(transcludedScope) {
+          return boundTranscludeFn(transcludedScope, scope);
+        }
+
         _.forEach(preLinkFns, function(linkFn) {
           linkFn(
             linkFn.isolateScope ? isolateScope : scope,
             $element,
             attrs,
             linkFn.require && getControllers(linkFn.require, $element),
-            childTranscludeFn
+            scopeBoundTranscludeFn
           );
         });
         if (childLinkFn) {
@@ -679,12 +698,14 @@ function $CompileProvider($provide) {
             $element,
             attrs,
             linkFn.require && getControllers(linkFn.require, $element),
-            childTranscludeFn
+            scopeBoundTranscludeFn
           );
         });
       }
       nodeLinkFn.terminal = terminal;
       nodeLinkFn.scope = newScopeDirective && newScopeDirective.scope;
+      nodeLinkFn.transcludeOnThisElement = hasTranscludeDirective;
+      nodeLinkFn.transclude = childTranscludeFn;
 
       return nodeLinkFn;
     }
