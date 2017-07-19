256bd34450a20725cd838e53752c6504f7fb6f68
Jacob Lambert <jacob.lambert@gmail.com>
2017-07-20 07:42:52 +0800

    Stringify expression function return values

--- src/interpolate.js
+++ src/interpolate.js
@@ -2,6 +2,16 @@
 
 var _ = require('lodash');
 
+function stringify(value) {
+    if (_.isNull(value) || _.isUndefined(value)) {
+        return '';
+    } else if (_.isObject(value)) {
+        return JSON.stringify(value);
+    } else {
+        return '' + value;
+    }
+}
+
 function $InterpolateProvider() {
 
     this.$get = ['$parse', function($parse) {
@@ -33,7 +43,7 @@ function $InterpolateProvider() {
             return function interpolateFn(context) {
                 return _.reduce(parts, function(result, part) {
                     if (_.isFunction(part)) {
-                        return result + part(context);
+                        return result + stringify(part(context));
                     } else {
                         return result + part;
                     }
