d04f14f26ecabf4fab04b2c5f1fc877eb7af5761
Jacob Lambert <jacob.lambert@gmail.com>
2017-07-24 14:02:44 +0800

    Refactor example app into components

--- example-app/app.js
+++ example-app/app.js
@@ -1,10 +1,21 @@
 angular.module('myExampleApp', [])
-    .controller('ExampleController', function() {
-        this.counter = 0;
-        this.increment = function() {
-            this.counter++;
-        };
-        this.decrement = function() {
-            this.counter--;
-        };
+    .component('exampleClicker', {
+        controller: function() {
+            this.counter = 0;
+            this.increment = function() {
+                this.counter++;
+            }
+            this.decrement = function() {
+                this.counter--;
+            }
+        },
+        transclude: true,
+        controllerAs: 'ctrl',
+        template: ` <div>
+                        <button ng-click="ctrl.increment()">+</button>
+                        CLICKER NUMBER <ng-transclude></ng-transclude>
+                        VALUE: {{ctrl.counter}}
+                        <button ng-click="ctrl.decrement()">-</button>
+                    </div>
+                  `
     });
